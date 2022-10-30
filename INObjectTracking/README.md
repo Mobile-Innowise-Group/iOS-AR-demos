f6da4aa7b17eaef11ebf81e9a0c60893924f2adc
Uladzislau Volchyk <uladzislau.volchuk@innowise-group.com>
2022-10-30 21:52:17 +0300

    feat: convert project to package

---
+++ INObjectTracking/README.md
@@ -0,0 +1,3 @@
+# INObjectTracking
+
+A description of this package.
---
+++ ObjectTrackingDemo/ObjectTrackingDemo/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  ObjectTrackingDemo
+//
+//  Created by Uladzislau Volchyk on 27/10/2022.
+//
+
+import SwiftUI
+import INObjectTracking
+
+struct ObjectTrackingWrap: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+//        ObjectTrackingFacade.buildFlow()!
+        ViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        uiViewController.view.frame = UIScreen.main.bounds
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ObjectTrackingWrap()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
